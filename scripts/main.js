2cf4ff3eb10da0e43e6bd50bf56dcafd3d02269e
Jesse Millar <hellojessemillar@gmail.com>
2016-06-20 11:56:23 -0600

    Styling some things and adding an autorefresh timer

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,15 @@
+var timeSinceRefresh = 0;
+
 setInterval(function() { // Autorefresh the page
     window.location.reload(true); // Force a pull from the server
 }, 5000);
+
+setInterval(function() { // Print when the page was last refreshed
+    timeSinceRefresh++;
+
+    if (timeSinceRefresh == 1) {
+        $("#refresh-text").text("refreshed " + timeSinceRefresh + " second ago");
+    } else {
+        $("#refresh-text").text("refreshed " + timeSinceRefresh + " seconds ago");
+    }
+}, 1000);
